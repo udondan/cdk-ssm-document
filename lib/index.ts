a0870403489a696d65ce0b042ff05fcfbc957b48
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-03-14 15:21:33 +0100

    parameter documentation

--- lib/index.ts
+++ lib/index.ts
@@ -5,44 +5,141 @@ import cdk = require('@aws-cdk/core');
 import yaml = require('js-yaml');
 import path = require('path');
 
+/**
+ * An SSM document parameter
+ */
 export interface DocumentParameter {
+
+    /**
+    *  Allowed values include the following: String, StringList, Boolean, Integer, MapList, and StringMap. To view examples of each type, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html#top-level-properties-type
+    */
     readonly type: string;
+
+    /**
+    * A description of the parameter
+    */
     readonly description: string;
+
+    /**
+    * The default value of the parameter or a reference to a parameter in Parameter Store
+    */
     readonly default?: any;
+
+    /**
+    * Allowed values for the parameter
+    */
     readonly allowedValues?: string[];
+
+    /**
+    * The regular expression the parameter must match
+    */
     readonly allowedPattern?: string;
+
+    /**
+    *  Used to display either a textfield or a textarea in the AWS console. textfield is a single-line text box. textarea is a multi-line text area
+    */
     readonly displayType?: string;
+
+    /**
+    * The minimum number of items allowed
+    */
     readonly minItems?: number;
+
+    /**
+    * The maximum number of items allowed
+    */
     readonly maxItems?: number;
+
+    /**
+    * The minimum number of parameter characters allowed
+    */
     readonly minChars?: number;
+
+    /**
+    * The maximum number of parameter characters allowed
+    */
     readonly maxChars?: number;
 }
 
-export interface DocumentMainSteps {
-    readonly action: string;
-    readonly name: string;
-    readonly inputs: {
-        [key: string]: any;
-    };
-    readonly precondition?: {
-        [key: string]: any;
-    };
+/**
+ * Steps include one or more actions, an optional precondition, a unique name of the action, and inputs (parameters) for those actions.
+ *
+ * For more information about documents, including information about creating documents and the differences between schema versions, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html
+ */
+export interface DocumentMainStep {
+    [key: string]: any;
 }
 
+/**
+ * The content of the SSM document. The syntax of your document is defined by the schema version used to create it.
+ *
+ * This module only supports schema version 2.2
+ *
+ * For details see https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-doc-syntax.html
+ */
 export interface DocumentContent {
+
+    /**
+    * The schema version to use, e.g. 2.2
+    */
     readonly schemaVersion: string;
+
+    /**
+    * Information you provide to describe the purpose of the document
+    */
     readonly description?: string;
-    readonly mainSteps: DocumentMainSteps[];
+
+    /**
+    * An object that can include multiple steps (plugins). Steps include one or more actions, an optional precondition, a unique name of the action, and inputs (parameters) for those actions.
+    *
+    * For more information about documents, including information about creating documents and the differences between schema versions, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html
+    */
+    readonly mainSteps: DocumentMainStep[];
+
+    /**
+    * The parameters the document accepts.
+    */
     readonly parameters?: {
         [key: string]: DocumentParameter;
     };
 }
 
+/**
+ * Definition of the SSM document
+ */
 export interface DocumentProps extends cdk.StackProps {
+
+    /**
+    * Defined if the default version should be updated to the latest version on document updates.
+    *
+    * @default true
+    */
     readonly updateDefaultVersion?: boolean;
+
+    /**
+    * Name of the document
+    *
+    * The name must be between 3 and 128 characters. Valid characters are a-z, A-Z, 0-9, and _, -, and . only
+    */
     readonly name: string;
+
+    /**
+    * Document type based on the service that you want to use.
+    *
+    * @default Command
+    */
     readonly documentType?: string;
+
+    /**
+    * Types of resources the document can run on. For example, `/AWS::EC2::Instance` or `/` for all resource types.
+    *
+    * @default /
+    */
     readonly targetType?: string;
+
+    /**
+    * Content of the SSM document. Can be passed as string or as object
+    */
     readonly content: string | DocumentContent;
 }
 
@@ -51,7 +148,13 @@ const ID = `CFN::Resource::${resourceType}`;
 const cleanID = ID.replace(/:+/g, '-');
 const lambdaTimeout = 3; // minutes
 
+/**
+ * An SSM document
+ */
 export class Document extends cdk.Construct {
+    /**
+    * Defines a new SSM document
+    */
     constructor(scope: cdk.Construct, id: string, props: DocumentProps) {
         super(scope, id);
 
@@ -73,7 +176,7 @@ export class Document extends cdk.Construct {
         const tags = props.tags || {};
         tags.CreatedBy = ID;
 
-        new cfn.CustomResource(this, `SSM-Document-${name}`, {
+        const document = new cfn.CustomResource(this, `SSM-Document-${name}`, {
             provider: cfn.CustomResourceProvider.fromLambda(fn),
             resourceType: resourceType,
             properties: {
@@ -86,6 +189,8 @@ export class Document extends cdk.Construct {
                 tags: tags,
             }
         });
+
+        return document;
     }
 
     private ensureLambda(): lambda.Function {
