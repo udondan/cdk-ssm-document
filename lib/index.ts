6f82cda38675e7a19ea5680ffb5394796883339f
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-06-25 14:33:54 +0200

    fmt

--- lib/index.ts
+++ lib/index.ts
@@ -9,55 +9,55 @@ import path = require('path');
  * An SSM document parameter
  */
 export interface DocumentParameter {
-    /**
-     *  Allowed values include the following: String, StringList, Boolean, Integer, MapList, and StringMap. To view examples of each type, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html#top-level-properties-type
-     */
-    readonly type: string;
+  /**
+   *  Allowed values include the following: String, StringList, Boolean, Integer, MapList, and StringMap. To view examples of each type, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html#top-level-properties-type
+   */
+  readonly type: string;
 
-    /**
-     * A description of the parameter
-     */
-    readonly description: string;
+  /**
+   * A description of the parameter
+   */
+  readonly description: string;
 
-    /**
-     * The default value of the parameter or a reference to a parameter in Parameter Store
-     */
-    readonly default?: any;
+  /**
+   * The default value of the parameter or a reference to a parameter in Parameter Store
+   */
+  readonly default?: any;
 
-    /**
-     * Allowed values for the parameter
-     */
-    readonly allowedValues?: string[];
+  /**
+   * Allowed values for the parameter
+   */
+  readonly allowedValues?: string[];
 
-    /**
-     * The regular expression the parameter must match
-     */
-    readonly allowedPattern?: string;
+  /**
+   * The regular expression the parameter must match
+   */
+  readonly allowedPattern?: string;
 
-    /**
-     *  Used to display either a textfield or a textarea in the AWS console. textfield is a single-line text box. textarea is a multi-line text area
-     */
-    readonly displayType?: string;
+  /**
+   *  Used to display either a textfield or a textarea in the AWS console. textfield is a single-line text box. textarea is a multi-line text area
+   */
+  readonly displayType?: string;
 
-    /**
-     * The minimum number of items allowed
-     */
-    readonly minItems?: number;
+  /**
+   * The minimum number of items allowed
+   */
+  readonly minItems?: number;
 
-    /**
-     * The maximum number of items allowed
-     */
-    readonly maxItems?: number;
+  /**
+   * The maximum number of items allowed
+   */
+  readonly maxItems?: number;
 
-    /**
-     * The minimum number of parameter characters allowed
-     */
-    readonly minChars?: number;
+  /**
+   * The minimum number of parameter characters allowed
+   */
+  readonly minChars?: number;
 
-    /**
-     * The maximum number of parameter characters allowed
-     */
-    readonly maxChars?: number;
+  /**
+   * The maximum number of parameter characters allowed
+   */
+  readonly maxChars?: number;
 }
 
 /**
@@ -66,7 +66,7 @@ export interface DocumentParameter {
  * For more information about documents, including information about creating documents and the differences between schema versions, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html
  */
 export interface DocumentMainStep {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 /**
@@ -77,67 +77,67 @@ export interface DocumentMainStep {
  * For details see https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-doc-syntax.html
  */
 export interface DocumentContent {
-    /**
-     * The schema version to use. Currently only version 2.2 is supported
-     */
-    readonly schemaVersion: string;
+  /**
+   * The schema version to use. Currently only version 2.2 is supported
+   */
+  readonly schemaVersion: string;
 
-    /**
-     * Information you provide to describe the purpose of the document
-     */
-    readonly description?: string;
+  /**
+   * Information you provide to describe the purpose of the document
+   */
+  readonly description?: string;
 
-    /**
-     * An object that can include multiple steps (plugins). Steps include one or more actions, an optional precondition, a unique name of the action, and inputs (parameters) for those actions.
-     *
-     * For more information about documents, including information about creating documents and the differences between schema versions, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html
-     */
-    readonly mainSteps: DocumentMainStep[];
+  /**
+   * An object that can include multiple steps (plugins). Steps include one or more actions, an optional precondition, a unique name of the action, and inputs (parameters) for those actions.
+   *
+   * For more information about documents, including information about creating documents and the differences between schema versions, see https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-plugins.html
+   */
+  readonly mainSteps: DocumentMainStep[];
 
-    /**
-     * The parameters the document accepts
-     */
-    readonly parameters?: {
-        [key: string]: DocumentParameter;
-    };
+  /**
+   * The parameters the document accepts
+   */
+  readonly parameters?: {
+    [key: string]: DocumentParameter;
+  };
 }
 
 /**
  * Definition of the SSM document
  */
 export interface DocumentProps extends cdk.StackProps {
-    /**
-     * Defines if the default version should be updated to the latest version on document updates
-     *
-     * @default true
-     */
-    readonly updateDefaultVersion?: boolean;
+  /**
+   * Defines if the default version should be updated to the latest version on document updates
+   *
+   * @default true
+   */
+  readonly updateDefaultVersion?: boolean;
 
-    /**
-     * Name of the document
-     *
-     * The name must be between 3 and 128 characters. Valid characters are a-z, A-Z, 0-9, and _, -, and . only
-     */
-    readonly name: string;
+  /**
+   * Name of the document
+   *
+   * The name must be between 3 and 128 characters. Valid characters are a-z, A-Z, 0-9, and _, -, and . only
+   */
+  readonly name: string;
 
-    /**
-     * Document type based on the service that you want to use
-     *
-     * @default Command
-     */
-    readonly documentType?: string;
+  /**
+   * Document type based on the service that you want to use
+   *
+   * @default Command
+   */
+  readonly documentType?: string;
 
-    /**
-     * Types of resources the document can run on. For example, `/AWS::EC2::Instance` or `/` for all resource types
-     *
-     * @default /
-     */
-    readonly targetType?: string;
+  /**
+   * Types of resources the document can run on. For example, `/AWS::EC2::Instance` or `/` for all resource types
+   *
+   * @default /
+   */
+  readonly targetType?: string;
 
-    /**
-     * Content of the SSM document. Can be passed as string or as object
-     */
-    readonly content: string | DocumentContent;
+  /**
+   * Content of the SSM document. Can be passed as string or as object
+   */
+  readonly content: string | DocumentContent;
 }
 
 const resourceType = 'Custom::SSM-Document';
@@ -149,146 +149,134 @@ const lambdaTimeout = 3; // minutes
  * An SSM document
  */
 export class Document extends cdk.Construct implements cdk.ITaggable {
-    /**
-     * Name of the document
-     */
-    public readonly name: string = '';
+  /**
+   * Name of the document
+   */
+  public readonly name: string = '';
 
-    /**
-     * Resource tags
-     */
-    public readonly tags: cdk.TagManager;
+  /**
+   * Resource tags
+   */
+  public readonly tags: cdk.TagManager;
 
-    /**
-     * Defines a new SSM document
-     */
-    constructor(scope: cdk.Construct, id: string, props: DocumentProps) {
-        super(scope, id);
+  /**
+   * Defines a new SSM document
+   */
+  constructor(scope: cdk.Construct, id: string, props: DocumentProps) {
+    super(scope, id);
 
-        this.tags = new cdk.TagManager(cdk.TagType.MAP, 'Custom::SSM-Document');
-        this.tags.setTag('CreatedBy', ID);
+    this.tags = new cdk.TagManager(cdk.TagType.MAP, 'Custom::SSM-Document');
+    this.tags.setTag('CreatedBy', ID);
 
-        const stack = cdk.Stack.of(this).stackName;
-        const fn = this.ensureLambda();
-        const name = this.fixDocumentName(props.name);
+    const stack = cdk.Stack.of(this).stackName;
+    const fn = this.ensureLambda();
+    const name = this.fixDocumentName(props.name);
 
-        if (name.length < 3 || name.length > 128) {
-            this.node.addError(
-                `SSM Document name ${name} is invalid. The name must be between 3 and 128 characters.`
-            );
-            return;
-        }
+    if (name.length < 3 || name.length > 128) {
+      this.node.addError(
+        `SSM Document name ${name} is invalid. The name must be between 3 and 128 characters.`
+      );
+      return;
+    }
 
-        let content = props.content;
+    let content = props.content;
 
-        if (typeof content === 'string') {
-            content = yaml.safeLoad(content);
-        }
+    if (typeof content === 'string') {
+      content = yaml.safeLoad(content);
+    }
 
-        const document = new cfn.CustomResource(this, `SSM-Document-${name}`, {
-            provider: cfn.CustomResourceProvider.fromLambda(fn),
-            resourceType: resourceType,
-            properties: {
-                updateDefaultVersion: props.updateDefaultVersion || true,
-                name: name,
-                content: content,
-                documentType: props.documentType || 'Command',
-                targetType: props.targetType || '/',
-                StackName: stack,
-                tags: cdk.Lazy.anyValue({
-                    produce: () => this.tags.renderTags(),
-                }),
+    const document = new cfn.CustomResource(this, `SSM-Document-${name}`, {
+      provider: cfn.CustomResourceProvider.fromLambda(fn),
+      resourceType: resourceType,
+      properties: {
+        updateDefaultVersion: props.updateDefaultVersion || true,
+        name: name,
+        content: content,
+        documentType: props.documentType || 'Command',
+        targetType: props.targetType || '/',
+        StackName: stack,
+        tags: cdk.Lazy.anyValue({
+          produce: () => this.tags.renderTags(),
+        }),
+      },
+    });
+
+    this.name = document.getAttString('Name');
+  }
+
+  private ensureLambda(): lambda.Function {
+    const stack = cdk.Stack.of(this);
+    const constructName = 'SSM-Document-Manager-Lambda';
+    const existing = stack.node.tryFindChild(constructName);
+    if (existing) {
+      return existing as lambda.Function;
+    }
+
+    const policy = new iam.ManagedPolicy(stack, 'SSM-Document-Manager-Policy', {
+      managedPolicyName: `${stack.stackName}-${cleanID}`,
+      description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing SSM documents`,
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['ssm:ListDocuments', 'ssm:ListTagsForResource'],
+          resources: ['*'],
+        }),
+        new iam.PolicyStatement({
+          actions: ['ssm:CreateDocument', 'ssm:AddTagsToResource'],
+          resources: ['*'],
+          conditions: {
+            StringEquals: {
+              'aws:RequestTag/CreatedBy': ID,
             },
-        });
+          },
+        }),
+        new iam.PolicyStatement({
+          actions: [
+            'ssm:DeleteDocument',
+            'ssm:DescribeDocument',
+            'ssm:GetDocument',
+            'ssm:ListDocumentVersions',
+            'ssm:ModifyDocumentPermission',
+            'ssm:UpdateDocument',
+            'ssm:UpdateDocumentDefaultVersion',
+            'ssm:AddTagsToResource',
+            'ssm:RemoveTagsFromResource',
+          ],
+          resources: ['*'],
+          conditions: {
+            StringEquals: {
+              'aws:ResourceTag/CreatedBy': ID,
+            },
+          },
+        }),
+      ],
+    });
 
-        this.name = document.getAttString('Name');
-    }
+    const role = new iam.Role(stack, 'SSM-Document-Manager-Role', {
+      roleName: `${stack.stackName}-${cleanID}`,
+      description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing SSM documents`,
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        policy,
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSLambdaBasicExecutionRole'
+        ),
+      ],
+    });
 
-    private ensureLambda(): lambda.Function {
-        const stack = cdk.Stack.of(this);
-        const constructName = 'SSM-Document-Manager-Lambda';
-        const existing = stack.node.tryFindChild(constructName);
-        if (existing) {
-            return existing as lambda.Function;
-        }
+    const fn = new lambda.Function(stack, constructName, {
+      functionName: `${stack.stackName}-${cleanID}`,
+      role: role,
+      description: 'Custom CFN resource: Manage SSM Documents',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/code.zip')),
+      timeout: cdk.Duration.minutes(lambdaTimeout),
+    });
 
-        const policy = new iam.ManagedPolicy(
-            stack,
-            'SSM-Document-Manager-Policy',
-            {
-                managedPolicyName: `${stack.stackName}-${cleanID}`,
-                description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing SSM documents`,
-                statements: [
-                    new iam.PolicyStatement({
-                        actions: [
-                            'ssm:ListDocuments',
-                            'ssm:ListTagsForResource',
-                        ],
-                        resources: ['*'],
-                    }),
-                    new iam.PolicyStatement({
-                        actions: [
-                            'ssm:CreateDocument',
-                            'ssm:AddTagsToResource',
-                        ],
-                        resources: ['*'],
-                        conditions: {
-                            StringEquals: {
-                                'aws:RequestTag/CreatedBy': ID,
-                            },
-                        },
-                    }),
-                    new iam.PolicyStatement({
-                        actions: [
-                            'ssm:DeleteDocument',
-                            'ssm:DescribeDocument',
-                            'ssm:GetDocument',
-                            'ssm:ListDocumentVersions',
-                            'ssm:ModifyDocumentPermission',
-                            'ssm:UpdateDocument',
-                            'ssm:UpdateDocumentDefaultVersion',
-                            'ssm:AddTagsToResource',
-                            'ssm:RemoveTagsFromResource',
-                        ],
-                        resources: ['*'],
-                        conditions: {
-                            StringEquals: {
-                                'aws:ResourceTag/CreatedBy': ID,
-                            },
-                        },
-                    }),
-                ],
-            }
-        );
+    return fn;
+  }
 
-        const role = new iam.Role(stack, 'SSM-Document-Manager-Role', {
-            roleName: `${stack.stackName}-${cleanID}`,
-            description: `Used by Lambda ${cleanID}, which is a custom CFN resource, managing SSM documents`,
-            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-            managedPolicies: [
-                policy,
-                iam.ManagedPolicy.fromAwsManagedPolicyName(
-                    'service-role/AWSLambdaBasicExecutionRole'
-                ),
-            ],
-        });
-
-        const fn = new lambda.Function(stack, constructName, {
-            functionName: `${stack.stackName}-${cleanID}`,
-            role: role,
-            description: 'Custom CFN resource: Manage SSM Documents',
-            runtime: lambda.Runtime.NODEJS_10_X,
-            handler: 'index.handler',
-            code: lambda.Code.fromAsset(
-                path.join(__dirname, '../lambda/code.zip')
-            ),
-            timeout: cdk.Duration.minutes(lambdaTimeout),
-        });
-
-        return fn;
-    }
-
-    private fixDocumentName(name: string): string {
-        return name.replace(/[^a-zA-Z0-9_.-]+/g, '-');
-    }
+  private fixDocumentName(name: string): string {
+    return name.replace(/[^a-zA-Z0-9_.-]+/g, '-');
+  }
 }
--- test/lib/test-stack.ts
+++ test/lib/test-stack.ts
@@ -5,60 +5,60 @@ import path = require('path');
 import { Document } from '../../lib';
 
 export class TestStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        let file = path.join(__dirname, '../documents/hello-world-yaml.yml');
-        new Document(this, 'SSM-Document-HelloWorld-Yaml', {
-            name: 'HelloWorld-from-yaml-file',
-            content: fs.readFileSync(file).toString(),
-        });
+    let file = path.join(__dirname, '../documents/hello-world-yaml.yml');
+    new Document(this, 'SSM-Document-HelloWorld-Yaml', {
+      name: 'HelloWorld-from-yaml-file',
+      content: fs.readFileSync(file).toString(),
+    });
 
-        file = path.join(__dirname, '../documents/hello-world-json.json');
-        new Document(this, 'SSM-Document-HelloWorld-Json', {
-            name: 'HelloWorld-from-json-file',
-            content: fs.readFileSync(file).toString(),
-        });
+    file = path.join(__dirname, '../documents/hello-world-json.json');
+    new Document(this, 'SSM-Document-HelloWorld-Json', {
+      name: 'HelloWorld-from-json-file',
+      content: fs.readFileSync(file).toString(),
+    });
 
-        new Document(this, 'SSM-Document-HelloWorld-Inline', {
-            name: 'HelloWorld-from-inline',
-            content: {
-                schemaVersion: '2.2',
-                description: 'Echo Hello World!',
-                parameters: {
-                    text: {
-                        default: 'Hello World!',
-                        description: 'Text to echo',
-                        type: 'String',
-                    },
-                },
-                mainSteps: [
-                    {
-                        name: 'echo',
-                        action: 'aws:runShellScript',
-                        inputs: {
-                            runCommand: ['echo "{{text}}"'],
-                        },
-                        precondition: {
-                            StringEquals: ['platformType', 'Linux'],
-                        },
-                    },
-                ],
+    new Document(this, 'SSM-Document-HelloWorld-Inline', {
+      name: 'HelloWorld-from-inline',
+      content: {
+        schemaVersion: '2.2',
+        description: 'Echo Hello World!',
+        parameters: {
+          text: {
+            default: 'Hello World!',
+            description: 'Text to echo',
+            type: 'String',
+          },
+        },
+        mainSteps: [
+          {
+            name: 'echo',
+            action: 'aws:runShellScript',
+            inputs: {
+              runCommand: ['echo "{{text}}"'],
             },
-        });
+            precondition: {
+              StringEquals: ['platformType', 'Linux'],
+            },
+          },
+        ],
+      },
+    });
 
-        const dir = path.join(__dirname, '../documents');
-        const files = fs.readdirSync(dir);
+    const dir = path.join(__dirname, '../documents');
+    const files = fs.readdirSync(dir);
 
-        for (const i in files) {
-            const name = files[i];
-            const shortName = name.split('.').slice(0, -1).join('.'); // removes file extension
-            const file = `${dir}/${name}`;
+    for (const i in files) {
+      const name = files[i];
+      const shortName = name.split('.').slice(0, -1).join('.'); // removes file extension
+      const file = `${dir}/${name}`;
 
-            new Document(this, `SSM-Document-Loop-${shortName}`, {
-                name: shortName,
-                content: fs.readFileSync(file).toString(),
-            });
-        }
+      new Document(this, `SSM-Document-Loop-${shortName}`, {
+        name: shortName,
+        content: fs.readFileSync(file).toString(),
+      });
     }
+  }
 }
